c8f3be85ace785d7be19e861817e7a379d74ef2b
Asmaa Hamdy <120820983+asmaaHamdym@users.noreply.github.com>
2024-01-11 23:33:35 +0200

    get User Input

--- swap.py
+++ swap.py
@@ -3,5 +3,7 @@ def swap(val1, val2):
     val1, val2 = val2, val1
     print(f'After swapping: {val1} {val2}')
 
+var1 = input('Enter the first value: ')
+var2 = input('Enter the second value: ')
 
-swap('s','k')
+swap(var1,var2)
