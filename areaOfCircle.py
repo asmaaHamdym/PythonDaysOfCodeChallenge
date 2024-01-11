30b135347bf32a36e441d850044669009df2a631
Asmaa Hamdy <120820983+asmaaHamdym@users.noreply.github.com>
2024-01-12 00:57:40 +0200

    Update areaOfCircle.py

--- areaOfCircle.py
+++ areaOfCircle.py
@@ -4,6 +4,6 @@ def calculateCircleArea(r):
     return math.pi * (r**2)
 
 
-radius = int(input('Please Enter the radius of your circle? '))
-area =  calculateCircleArea(radius)
+radius = float(input('Please Enter the radius of your circle? '))
+area =  round(calculateCircleArea(radius) , 2)
 print(f'The area of a circle with radius of {radius}  is {area}')
