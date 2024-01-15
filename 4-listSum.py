76a136547d86747c7f61f17be36f77f0ccd59cab
Asmaa Hamdy <120820983+asmaaHamdym@users.noreply.github.com>
2024-01-15 07:55:38 +0200

    added function

--- 4-listSum.py
+++ 4-listSum.py
@@ -1,13 +1,14 @@
 # Write a program to find the sum of all elements in a list
+def listSum(arr):
+    if not arr:
+        print('Enter the elements')
+    else:
+        try:
+            arr = [float(element) for element in arr]
+            print(f'The sum of all elements is {sum(arr)}')
+        except:
+            print('The list has invalid values')
 
 
 list = input('Please enter the numbers of the list separated by a space: ').strip().split()
-if not list:
-    print('Enter the elements')
-else:
-    try:
-        list = [float(element) for element in list]
-        print(f'The sum of all elements is {sum(list)}')
-    except:
-        print('The list has invalid values')
-
+listSum(list)
