19e89545006d5ef88b7f1e4c4d1b10f63dc7adf9
Asmaa Hamdy <120820983+asmaaHamdym@users.noreply.github.com>
2024-01-15 07:37:06 +0200

    float fix

--- 4-listSum.py
+++ 4-listSum.py
@@ -6,7 +6,7 @@ if not list:
     print('Enter the elements')
 else:
     try:
-        list = [int(element) for i, element in enumerate(list)]
+        list = [float(element) for element in list]
         print(f'The sum of all elements is {sum(list)}')
     except:
         print('The list has invalid values')
