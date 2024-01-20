282e10d63efd579e64356b809a4809b3b2eb67eb
Asmaa Hamdy <120820983+asmaaHamdym@users.noreply.github.com>
2024-01-20 15:44:52 +0200

    day8

--- 8-noOfUppercaseLowercase.py
+++ 8-noOfUppercaseLowercase.py
@@ -1,1 +1,22 @@
 #Write a function that accepts a string and calculates the number of uppercase and lowercase letters in it
+def caseCounter(word):
+    upperCount, lowerCount = 0, 0
+
+    for letter in word:
+        if letter.islower():
+            upperCount += 1
+        else:
+            lowerCount += 1
+    
+    return [lowerCount, upperCount]
+
+while True:
+    newWord = input('Enter the word: ').strip()
+    if newWord.isalpha():
+        break
+    newWord = print('error! please enter a valid string!')
+
+
+print(f'Number of lower case letters in {newWord} is {caseCounter(newWord)[0]}.')
+
+print(f'Number of upper case letters in {newWord} is {caseCounter(newWord)[1]}.')
