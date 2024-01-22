b1667ff32309f84b4de4b53e16bed3400e6aaed6
Asmaa Hamdy <120820983+asmaaHamdym@users.noreply.github.com>
2024-01-22 12:16:42 +0200

    day11

---
+++ 11-numberMultiplicationTable.py
@@ -0,0 +1,21 @@
+# Write a program to print the multiplication table of a given number.
+
+from pkg_resources import DistInfoDistribution
+
+
+def is_int(n):
+    try:
+        int(n)
+        return True
+    except ValueError:
+        return False
+
+
+while True:
+    number = input('Your number: ').strip()
+    if is_int(number):
+        break
+    print("Enter a valid integer!")
+
+for digit in range(1, int(number) + 1):
+    print(f'{digit} x {number} = {int(number) * digit}')
