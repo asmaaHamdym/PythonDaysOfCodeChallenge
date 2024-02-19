91bc8ef763ba9f1b4c6f65d49fa442864474aa78
Asmaa Hamdy <120820983+asmaaHamdym@users.noreply.github.com>
2024-02-19 21:48:10 +0200

    day 29 finish

--- 29-GenerateRandomNo.py
+++ 29-GenerateRandomNo.py
@@ -1,1 +1,16 @@
 #Create a function that generates a random number between a given range.
+from os import rmdir
+import random
+
+def randomGenerator(rMin,rMax):
+    return random.randint(rMin, rMax)
+
+
+
+def main():
+    ...
+
+
+
+if __name__ == "__main__":
+    main()
