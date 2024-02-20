49ca1d6cbfbb721804cbe5b34a3722c7a8dd0ba6
Asmaa Hamdy <120820983+asmaaHamdym@users.noreply.github.com>
2024-02-20 08:24:46 +0200

    improved the logic on the main function

--- 29-GenerateRandomNo.py
+++ 29-GenerateRandomNo.py
@@ -5,10 +5,23 @@ import random
 def randomGenerator(rMin,rMax):
     return random.randint(rMin, rMax)
 
-
+def checkRange(start, end):
+    if not start.isdigit() or not end.isdigit():
+        return "These values are incorrect!"
+    if int(start) > int(end):
+        return "The start value cannot be greater than the end"
+    return ''
 
 def main():
-    ...
+    while True:
+        numberRange = input("Enter the start and the end of the range separated by a space: ").strip().split()
+        minVal, maxVal = numberRange
+        errMsg = checkRange(minVal, maxVal) 
+        if not errMsg:
+            print(f'The random value is: {randomGenerator(int(minVal),int(maxVal))}.')
+            break
+        else:
+            print(errMsg)
 
 
 
